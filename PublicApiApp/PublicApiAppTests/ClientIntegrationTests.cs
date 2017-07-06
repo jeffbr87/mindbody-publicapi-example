62d63c2c7ad86096d82fc5db6b87b2925d85f6c5
Chris Volpi <christopher.volpi@mindbodyonline.com>
2017-07-05 17:00:47 -0700

    Moved Client Repo tests into their own folder

--- PublicApiApp/PublicApiAppTests/ClientIntegrationTests.cs
+++ PublicApiApp/PublicApiAppTests/ClientIntegrationTests.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PublicApiApp.Engines;
-using PublicApiApp.Repositories;
 
 namespace PublicApiAppTests
 {
@@ -14,59 +12,5 @@ namespace PublicApiAppTests
             ClientEngine clientEngine = new ClientEngine();
             clientEngine.GetClients();
         }
-
-        #region AddOrUpdateClients Tests
-
-        [TestMethod]
-        public void UpdateClientTest()
-        {
-            var clientId = "100014871";
-            var randNum = new Random().Next(1, 101);
-            var firstName = $"Test First {randNum}";
-            var lastName = $"Test Last {randNum}";
-            var email = $"volpmbotest1+{randNum}@gmail.com";
-
-            var client = new Client
-            {
-                ID = clientId,
-                FirstName = firstName,
-                LastName = lastName,
-                Email = email
-            };
-
-            var clientRepository = new ClientRepository();
-            var result = clientRepository.AddOrUpdateClients(client, true);
-
-            Assert.AreEqual(firstName, result.FirstName);
-            Assert.AreEqual(lastName, result.LastName);
-            Assert.AreEqual(email, result.Email);
-            Assert.AreEqual(ActionCode.Updated, result.Action);
-        }
-
-        [TestMethod]
-        public void AddClientTest()
-        {
-            var randNum = new Random().Next(1, 101);
-            var firstName = $"Test First {randNum}";
-            var lastName = $"Test Last {randNum}";
-            var email = $"volpmbotest1+{randNum}@gmail.com";
-
-            var client = new Client
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                Email = email
-            };
-
-            var clientRepository = new ClientRepository();
-            var result = clientRepository.AddOrUpdateClients(client, true);
-
-            Assert.AreEqual(firstName, result.FirstName);
-            Assert.AreEqual(lastName, result.LastName);
-            Assert.AreEqual(email, result.Email);
-            Assert.AreEqual(ActionCode.Added, result.Action);
-        }
-
-        #endregion
     }
 }
