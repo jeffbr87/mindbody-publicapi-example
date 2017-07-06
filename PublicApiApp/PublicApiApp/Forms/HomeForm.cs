5ffc06d0a941a29ace12ef6f56540537c7ae3e00
SLO\chantel.crother <chantel.crother@mindbodyonline.com>
2017-07-06 14:40:24 -0700

    Added in add client functionality. Home form now reloads after client is added/updated. Clients are sorted by first name.

--- PublicApiApp/PublicApiApp/Forms/HomeForm.cs
+++ PublicApiApp/PublicApiApp/Forms/HomeForm.cs
@@ -18,7 +18,7 @@ namespace PublicApiApp
         private readonly ClientEngine _clientEngine = new ClientEngine();
         private readonly SalesEngine _salesEngine = new SalesEngine();
 
-        List<Client> clients = new List<Client>();
+        List<Client> _clients = new List<Client>();
 
         public HomeForm()
         {
@@ -28,14 +28,9 @@ namespace PublicApiApp
         private void getClientsAndClasses_Click(object sender, EventArgs e)
         {
 
-        }
+        }     
 
-        private void addClient_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void HomeForm_Load(object sender, EventArgs e)
+        public void HomeForm_Load(object sender, EventArgs e)
         {
             //Sales
             var sales = _salesEngine.GetSales(DateTime.Now.Date, DateTime.Now);
@@ -63,8 +58,8 @@ namespace PublicApiApp
             clientList.Columns.Add("First Name", 100);
             clientList.Columns.Add("Last Name", 100);
             clientList.Columns.Add("Email", 100);
-            clients = _clientEngine.GetClients().ToList();
-            foreach (var client in clients)
+            _clients = _clientEngine.GetClients().OrderBy(c => c.FirstName).ToList();
+            foreach (var client in _clients)
             {
                 ListViewItem item = new ListViewItem(new []{ client.FirstName, client.LastName, client.Email, client.ID });
                 clientList.Items.Add(item);
@@ -99,12 +94,18 @@ namespace PublicApiApp
             }
         }
 
+        private void addClient_Click(object sender, EventArgs e)
+        {
+            var updateFrm = new AddOrUpdateForm(_clientEngine, this);
+            updateFrm.Show();
+        }
+
         private void updateClient_Click(object sender, EventArgs e)
         {
             var selectedClient = clientList.SelectedItems[0];
             var clientId = selectedClient.SubItems[3].Text;           
-            var client = clients.Single(c => c.ID == clientId);
-            var updateFrm = new AddOrUpdateForm(client, _clientEngine);
+            var client = _clients.Single(c => c.ID == clientId);
+            var updateFrm = new AddOrUpdateForm(client, _clientEngine, this);
             updateFrm.Show();
         }
     }
